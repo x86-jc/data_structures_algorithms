88e88a606ffefa505182b88a9a1d07d05a101dd4
John Coleman <148596003+x86-jc@users.noreply.github.com>
2026-04-29 19:25:33 -0600

    Create avl_height.h

---
+++ avl/avl_height.h
@@ -0,0 +1,18 @@
+/************************************************
+ * Title:               avl                     *
+ * Creator:             John J. Coleman         *
+ * Creation Date:       2026.04.28              *
+ * Modified Date:       2026.04.29              *
+ *                                              *
+ * Description:                                 *
+ * AVL tree example.                            *
+ ************************************************/
+
+#ifndef AVL_HEIGHT_H
+#define AVL_HEIGHT_H
+
+#include "avl.h"
+
+int node_height(struct node *n);
+
+#endif
