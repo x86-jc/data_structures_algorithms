78aae8bd1c8238139a9c65f3990035fc20452837
John Coleman <148596003+x86-jc@users.noreply.github.com>
2026-04-29 19:23:36 -0600

    Create avl_balance.h

---
+++ avl/avl_balance.h
@@ -0,0 +1,18 @@
+/************************************************
+ * Title:               avl                     *
+ * Creator:             John J. Coleman         *
+ * Creation Date:       2026.04.28              *
+ * Modified Date:       2026.04.29              *
+ *                                              *
+ * Description:                                 *
+ * AVL tree example.                            *
+ ************************************************/
+
+#ifndef AVL_BALANCE_H
+#define AVL_BALANCE_H
+
+#include "avl.h"
+
+int balance(struct node *n);
+
+#endif
